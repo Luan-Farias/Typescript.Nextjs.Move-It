f1e4a156d22bac9429a3b7c7d52300ddb7388598
Luan Farias <luan.farias_bvs@outlook.com>
2021-02-23 13:16:59 -0300

    Completed Challenges and Profile components maden

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
+import styles from '../styles/pages/home.module.css';
 import { ExperienceBar } from '../components/ExperienceBar';
-import Head from 'next/head';
+import { Profile } from '../components/Profile';
+import { CompletedChallenges } from '../components/CompletedChallenges';
 
 export default function Home() {
     return (
-        <div className="container">
+        <div className={styles.container}>
             <ExperienceBar /> 
+
+            <section>
+                <div>
+                    <Profile />
+                    <CompletedChallenges />
+                </div>
+                <div>
+
+                </div>
+            </section>
         </div>
     );
 }
